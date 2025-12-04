f96f4db49488e86ad48c03982d458b83ea318b63
alexw1230 <alexanderwiegand310@gmail.com>
2025-12-04 00:54:53 -0500

    ye

--- codes/SpatialTree.h
+++ codes/SpatialTree.h
@@ -4,23 +4,20 @@
 #include "Drone.h"
 #include <iostream>
 
-// Page 4: Code Example "class SpatialNode" [cite: 136]
-// We use a struct for public access simplicity within the tree logic
-struct SpatialNode {
-    Drone* drone;       // Page 4: "Drone* drone;" [cite: 138]
-    SpatialNode* left;  // Page 4: "SpatialNode left;" (Corrected to pointer) 
-    SpatialNode* right; // Page 4: "SpatialNode right;" (Corrected to pointer) [cite: 140]
 
-    // Page 4: Constructor logic [cite: 141]
+struct SpatialNode {
+    Drone* drone;       
+    SpatialNode* left;  
+    SpatialNode* right;
+
     SpatialNode(Drone* d) : drone(d), left(nullptr), right(nullptr) {}
 };
 
-// Page 4: Code Example "class SpatialTree"
 class SpatialTree {
 private:
-    SpatialNode* root; // Page 4: "SpatialNode root;" (Corrected to pointer) 
+    SpatialNode* root; 
 
-    // Helper functions for recursion (Standard C++ Tree implementation practice)
+    // Helper functions for recursion
     void insertRec(SpatialNode*& node, Drone* drone);
     Drone* searchRec(SpatialNode* node, int x, int y);
     void inOrderRec(SpatialNode* node);
@@ -28,15 +25,11 @@ private:
     void postOrderRec(SpatialNode* node);
 
 public:
-    SpatialTree(); // Constructor
+    SpatialTree();
 
-    // Page 4: "void insert(Drone* drone);"
     void insert(Drone* drone);
-
-    // Page 4: "Drone* search(int x, int y);"
     Drone* search(int x, int y);
 
-    // Page 4: Traversal methods 
     void traverseInOrder();
     void traversePreOrder();
     void traversePostOrder();
